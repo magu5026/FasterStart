a2c06e7c27ba09fe92e32feb4c194c4d9782351d
magu5026 <magu5026@gmail.com>
2020-12-19 23:08:00 +0100

    [0.16.1] - Update for Factorio 0.16

--- control.lua
+++ control.lua
@@ -1,13 +1,11 @@
 script.on_event(defines.events.on_player_created, function(event)
 	local player = game.players[event.player_index]
-	if (not player.character or not player.get_inventory(defines.inventory.player_armor)) then
-		return
-	end
+	if not (player.character and player.get_inventory(defines.inventory.player_armor)) then return end
 	player.insert{name="mini-power-armor", count = 1}
 	local A_Grid = player.get_inventory(defines.inventory.player_armor)[1].grid
 		  A_Grid.put({name = "mini-fusion-reactor-equipment"})
 		  A_Grid.put({name = "night-vision-equipment"})
 		  A_Grid.put({name = "exoskeleton-equipment"})
 		  A_Grid.put({name = "personal-roboport-mk2-equipment"})
-	player.insert{name="fusion-construction-robot", count = 25}
+	player.insert({name="fusion-construction-robot", count = 25})
 end)
--- data.lua
+++ data.lua
@@ -1,1 +1,68 @@
-require("prototypes.faststart")
+data:extend(
+{
+	{
+        type = "equipment-grid",
+		name = "mini-equipment-grid",
+		width = 6,
+		height = 4,
+		equipment_categories = {"armor"}
+	}
+})
+
+
+local fusion_robot_item = table.deepcopy(data.raw['item']['construction-robot'])
+fusion_robot_item.name = "fusion-construction-robot"
+fusion_robot_item.icon = "__FasterStart__/graphics/icons/fusion-construction-robot.png"
+fusion_robot_item.place_result = "fusion-construction-robot"
+
+
+local mini_fusion_reactor_item = table.deepcopy(data.raw['item']['fusion-reactor-equipment'])
+mini_fusion_reactor_item.name = "mini-fusion-reactor-equipment"
+mini_fusion_reactor_item.icon = "__FasterStart__/graphics/icons/mini-fusion-reactor-equipment.png"
+mini_fusion_reactor_item.placed_as_equipment_result = "mini-fusion-reactor-equipment"
+
+
+local mini_armor = table.deepcopy(data.raw['armor']['modular-armor'])
+mini_armor.name = "mini-power-armor"
+mini_armor.icon = "__FasterStart__/graphics/icons/mini-power-armor.png"
+mini_armor.resistances = {}
+mini_armor.durability = 5000
+mini_armor.equipment_grid = "mini-equipment-grid"
+
+
+local mini_fusion_reactor = table.deepcopy(data.raw['generator-equipment']['fusion-reactor-equipment'])
+mini_fusion_reactor.name = "mini-fusion-reactor-equipment"
+mini_fusion_reactor.sprite.filename = "__FasterStart__/graphics/equipment/mini-fusion-reactor-equipment.png"
+mini_fusion_reactor.sprite.width = 65
+mini_fusion_reactor.shape.width = 2
+mini_fusion_reactor.power = "250kW"
+
+
+local fusion_robot = table.deepcopy(data.raw['construction-robot']['construction-robot'])
+fusion_robot.name = "fusion-construction-robot"
+fusion_robot.icon = "__FasterStart__/graphics/icons/fusion-construction-robot.png"
+fusion_robot.minable.result = "fusion-construction-robot"
+fusion_robot. energy_per_tick = "0kJ"
+fusion_robot.energy_per_move = "0kJ"
+fusion_robot.idle.filename = "__FasterStart__/graphics/entity/construction-robot-nuclear.png"
+fusion_robot.idle.hr_version.filename = "__FasterStart__/graphics/entity/hr-construction-robot.png"
+fusion_robot.in_motion.filename = "__FasterStart__/graphics/entity/construction-robot-nuclear.png"
+fusion_robot.in_motion.hr_version.filename = "__FasterStart__/graphics/entity/hr-construction-robot.png"
+fusion_robot.shadow_idle.filename = "__FasterStart__/graphics/entity/construction-robot-nuclear-shadow.png"
+fusion_robot.shadow_idle.hr_version.filename = "__FasterStart__/graphics/entity/hr-construction-robot-shadow.png"
+fusion_robot.shadow_in_motion.filename = "__FasterStart__/graphics/entity/construction-robot-nuclear-shadow.png"
+fusion_robot.shadow_in_motion.hr_version.filename = "__FasterStart__/graphics/entity/hr-construction-robot-shadow.png"
+fusion_robot.working.filename = "__FasterStart__/graphics/entity/construction-robot-nuclear-working.png"
+fusion_robot.working.hr_version.filename = "__FasterStart__/graphics/entity/hr-construction-robot-working.png"
+fusion_robot.shadow_working.stripes = 
+	util.multiplystripes(2, 
+		{
+			{
+				filename = "__FasterStart__/graphics/entity/construction-robot-nuclear-shadow.png",
+				width_in_frames = 16,
+				height_in_frames = 1,
+			}
+		})
+		
+		
+data:extend({fusion_robot_item,mini_fusion_reactor_item,mini_armor,mini_fusion_reactor,fusion_robot})
