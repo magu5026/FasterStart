ed1ebb0c0e62675715fba2173ded954c3a260d43
magu5026 <magu5026@gmail.com>
2020-12-19 23:12:49 +0100

    [0.17.3] - Fixed the version issue
    You have to update it manually. I'm sorry :(

--- control.lua
+++ control.lua
@@ -1,12 +1,11 @@
 local MODNAME = "FasterStart"
 
-local function Init()
-	global.FasterStart = global.FasterStart or {}
-	global.FasterStart.PlayerList = global.FasterStart.PlayerList or {}
+function init()
+	global.PlayerList = global.PlayerList or {}
 end
 
 function ON_INIT()
-	Init()
+	init()
 
 	for _,player in pairs(game.players) do
 		if player and player.connected then
@@ -16,23 +15,12 @@ function ON_INIT()
 end
 
 function ON_CONFIGURATION_CHANGED(data)
-	Init()
-	
+	init()
+
 	if NeedMigration(data,MODNAME) then
-		local old_version = GetOldVersion(data,MODNAME)
-		if old_version < "00.16.03" then
-			for _,player in pairs(game.players) do
-				if player.character then
-					local player_armor = player.get_inventory(defines.inventory.player_armor)
-					local player_main = player.get_inventory(defines.inventory.player_main)
-					if (player_armor.find_item_stack("mini-power-armor") or player_main.find_item_stack("fusion-construction-robot")) then
-						table.insert(global.FasterStart.PlayerList,player)
-					else
-						create_armor(player)
-					end				
-				end
-			end
-		end
+		local old_players = global.FasterStart.PlayerList
+		global = {}
+		global.PlayerList = old_players
 	end
 end	
 
@@ -43,21 +31,35 @@ end
 
 function create_armor(player)
 	if Exists(player) then return end
-	if (not player.character or not player.get_inventory(defines.inventory.player_armor)) then return end
 	
-	player.insert{name="mini-power-armor", count = 1}
-	local A_Grid = player.get_inventory(defines.inventory.player_armor)[1].grid
-		  A_Grid.put({name = "mini-fusion-reactor-equipment"})
-		  A_Grid.put({name = "night-vision-equipment"})
-		  A_Grid.put({name = "exoskeleton-equipment"})
-		  A_Grid.put({name = "personal-roboport-mk2-equipment"})
-	player.insert{name="fusion-construction-robot", count = 25}
-		
-	table.insert(global.FasterStart.PlayerList,player)
+	local armor_inventory = player.get_inventory(defines.inventory.player_armor)
+	local player_inventory = player.get_inventory(defines.inventory.player_main)
+	
+	if (not player.character or not armor_inventory) then return end
+	
+	local contents = armor_inventory.get_contents()
+	local armor
+	
+	if Count(contents) > 0 then 
+		player_inventory.insert({name="mini-power-armor"})
+		armor = player_inventory.find_item_stack("mini-power-armor")
+	else
+		armor_inventory.insert({name="mini-power-armor"})
+		armor = armor_inventory.find_item_stack("mini-power-armor")
+	end
+	
+	local a_grid = armor.grid
+		a_grid.put({name = "mini-fusion-reactor-equipment"})
+		a_grid.put({name = "night-vision-equipment"})
+		a_grid.put({name = "exoskeleton-equipment"})
+		a_grid.put({name = "personal-roboport-mk2-equipment"})
+	player_inventory.insert{name="fusion-construction-robot", count = 25}
+			
+	table.insert(global.PlayerList,player)
 end
 
 function Exists(player)
-	for _,item in pairs(global.FasterStart.PlayerList) do
+	for _,item in pairs(global.PlayerList) do
 		if item == player then return true end
 	end
 	return false
@@ -79,6 +81,14 @@ end
 
 function FormatVersion(version)
 	return string.format("%02d.%02d.%02d", string.match(version, "(%d+).(%d+).(%d+)"))
+end
+
+function Count(list)
+	local i = 0
+	for _ in pairs(list) do
+		i = i + 1
+	end
+	return i
 end
 
 script.on_init(ON_INIT)
